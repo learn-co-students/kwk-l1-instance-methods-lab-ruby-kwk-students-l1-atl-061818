20d13347745e534a90d7dc766d660ed2b3266b16
lxurel <laurelblase@gmail.com>
2018-06-22 13:20:57 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,20 @@
 # dog.rb
+
+class Dog 
+  
+  def initialize(bark,sit)
+    @bark = bark
+    @sit = sit
+  end
+  
+  def bark
+    puts "Woof!"
+  end
+  
+  def sit
+    puts "The dog is sitting."
+  end
+  
+    
+
+    
--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,17 @@
 # person.rb
+
+class Person
+  
+  def initialize(talk,walk)
+    @talk = talk
+    @walk = walk
+  end
+  
+  def talk 
+    puts "Hello World!"
+  end
+  
+  def walk 
+    puts "The person is walking."
+  end 
+  
